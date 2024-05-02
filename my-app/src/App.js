7cea7f4cc208215a6c10afde8214fd405b02e38c
Vinicius Souza <vinicipkt@live.com>
2024-05-02 07:28:58 -0300

    Voltando

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
-import Led from './Components/Led';
+
 
 
 function App() {
 
-  const [ligado, setLigado] = useState(false);
+  const [cor, setCor] = useState(1);
+
+  const vermelho = { color: '#f00' };
+  const verde = { color: '#0f0' };
+  const azul = { color: '#00f' };
+
+  const retornaCor = (c) => {
+    if (c == 1) {
+      return vermelho;
+    } else if (c == 2) {
+      return verde;
+    } else {
+      return azul;
+    }
+  };
+
+  const mudacor = () => {
+    setCor(cor + 1);
+    if (cor > 2) {
+      setCor(1);
+    }
+  };
+
+  // setInterval(mudacor, 1000);
 
   return (
     <>
-      <Led />
+      <h1 style={retornaCor(cor)}>Cores</h1>
+      <button onClick={mudacor}>Muda cor</button>
     </>
   );
-}
+};
 
 export default App;
