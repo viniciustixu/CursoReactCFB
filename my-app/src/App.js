f8b831a881bab4b3fbad0e324e85d58b733f33e2
Vinicius Souza <vinicipkt@live.com>
2024-05-08 10:47:05 -0300

    Listas usando map

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,35 +6,15 @@ import React, { useState } from 'react';
 
 function App() {
 
-  const [cor, setCor] = useState(1);
-
-  const vermelho = { color: '#f00' };
-  const verde = { color: '#0f0' };
-  const azul = { color: '#00f' };
-
-  const retornaCor = (c) => {
-    if (c == 1) {
-      return vermelho;
-    } else if (c == 2) {
-      return verde;
-    } else {
-      return azul;
-    }
-  };
-
-  const mudacor = () => {
-    setCor(cor + 1);
-    if (cor > 2) {
-      setCor(1);
-    }
-  };
-
-  // setInterval(mudacor, 1000);
+  const carros = ['HRV', 'Golf', 'Focus', 'Cruze', 'Argo'];
+  const listaCarros = carros.map(
+    (c, i) =>
+      <li key={i}>{i} - {c}</li>
+  );
 
   return (
     <>
-      <h1 style={retornaCor(cor)}>Cores</h1>
-      <button onClick={mudacor}>Muda cor</button>
+      <ul> {listaCarros} </ul>
     </>
   );
 };
