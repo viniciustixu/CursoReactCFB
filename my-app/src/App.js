3f66b99a84df1fa03d8cfc0b63a30ff07a013f1f
Vinicius Souza <vinicipkt@live.com>
2024-05-11 10:07:47 -0300

    Manipulando objetos com State

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,25 +6,32 @@ import React, { useState } from 'react';
 
 function App() {
 
-  const [nome, setNome] = useState('');
-  const [carro, setCarro] = useState('Golf');
+  const [form, setForm] = useState({ "nome": "", "curso": "", "ano": "" });
 
-
+  const handleFormChange = (e) => {
+    if (e.target.getAttribute('name') == 'fNome') {
+      setForm({ "nome": e.target.value, "curso": form.curso, "ano": form.ano });
+    } else if (e.target.getAttribute('name') == 'fCurso') {
+      setForm({ "nome": form.nome, "curso": e.target.value, "ano": form.ano });
+    } else if (e.target.getAttribute('name') == 'fAno') {
+      setForm({ "nome": form.nome, "curso": form.curso, "ano": e.target.value });
+    }
+  };
 
   return (
     <>
-      <label htmlFor="fnome">Digite seu nome</label>
-      <input type="text" name="fnome" id="fnome" value={nome} onChange={(e) => setNome(e.target.value)} />
-      <p>Nome digitado: {nome} </p>
+      <label htmlFor="formNome">Nome</label>
+      <input type="text" name="fNome" id="formNome" value={form.nome} onChange={(e) => handleFormChange(e)} />
 
-      <label htmlFor="sCarro">Selecione um carro</label>
-      <select value={carro} id="sCarro" onChange={(e) => setCarro(e.target.value)}>
-        <option value="HRV">HRV</option>
-        <option value="Golf">Golf</option>
-        <option value="Cruze">Cruze</option>
-        <option value="Argo">Argo</option>
-      </select>
-      <p>Carro selecionado: {carro}</p>
+      <label htmlFor="formCurso">Curso</label>
+      <input type="text" name="fCurso" id="formCurso" value={form.curso} onChange={(e) => handleFormChange(e)} />
+
+      <label htmlFor="formAno">Ano</label>
+      <input type="text" name="fAno" id="formAno" value={form.ano} onChange={(e) => handleFormChange(e)} />
+
+      <p>Nome digitado: {form.nome} </p>
+      <p>Curso digitado: {form.curso} </p>
+      <p>Ano digitado: {form.ano} </p>
     </>
   );
 };
