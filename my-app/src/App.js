974e05166db878a4a899c4d67bc84cef6e3741de
Vinicius Souza <vinicipkt@live.com>
2024-05-15 09:56:12 -0300

    Componentes de classe

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
-
+import Classe from './Components/Classe';
 
 
 function App() {
 
-  const [nome, setNome] = useState();
-
-  const armazenar = (chave, valor) => {
-    localStorage.setItem(chave, valor);
-  };
-
-  const consultar = (chave) => {
-    alert(localStorage.getItem(chave));
-  };
-
-  const apagar = (chave) => {
-    localStorage.removeItem(chave);
-  };
-
-
   return (
     <>
-      <label htmlFor="iNome">Digite um nome: </label>
-      <input type="text" id="iNome" value={nome} onChange={(e) => setNome(e.target.value)} />
-      <button onClick={() => armazenar('ls_nome', nome)}>Gravar nome</button>
-      <button onClick={() => consultar('ls_nome')}>Ver nome</button>
-      <button onClick={() => apagar('ls_nome')}>Remover nome</button>
+      <Classe canal="CFB Cursos" curso="Curso de React" />
+      <h1>Componentes de classe</h1>
     </>
   );
 };
