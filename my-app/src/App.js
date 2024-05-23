d173a56d50e523877bbdb38d29389164a6312a0c
Vinicius Souza <vinicipkt@live.com>
2024-05-23 12:36:09 -0300

    Praticando

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,49 +1,73 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Pagina1 from './Components/Pagina1';
-import Pagina2 from './Components/Pagina2';
+
+
+
+const carros = [
+  { categoria: "Esporte", preco: "110000", modelo: "Golf GTI" },
+  { categoria: "Esporte", preco: "135000", modelo: "Camaro" },
+  { categoria: "SUV", preco: "70000", modelo: "HRV" },
+  { categoria: "SUV", preco: "85000", modelo: "T-Cross" },
+  { categoria: "Utilitário", preco: "90000", modelo: "Hillux" },
+  { categoria: "Utilitário", preco: "65000", modelo: "Ranger" },
+];
+
+const linhas = (cat) => {
+  const li = [];
+  carros.forEach((carro, index) => {
+    if (carro.categoria.toLocaleUpperCase().includes(cat.toLocaleUpperCase()) || cat === "") {
+      li.push(
+        <tr>
+          <td>{carro.categoria}</td>
+          <td>{carro.preco}</td>
+          <td>{carro.modelo}</td>
+        </tr>
+      );
+    }
+  });
+
+  return li;
+};
+
+
+const TabelaCarros = (cat) => {
+  return (
+    <table border='1' style={{ borderCollapse: 'collapse' }}>
+      <thead>
+        <tr>
+          <th>Categoria</th>
+          <th>Preço</th>
+          <th>Modelo</th>
+        </tr>
+      </thead>
+      <tbody>
+        {linhas(cat)}
+      </tbody>
+    </table>
+  );
+};
+
+const pesquisaCategoria = (cat, scat) => {
+  return (
+    <div>
+      <label htmlFor="idPesquisa">Digite uma categoria</label>
+      <input type="text" id="idPesquisa" value={cat} onChange={(e) => scat(e.target.value)} />
+    </div>
+  );
+
+};
+
 
 
 function App() {
 
-  const [pagina, setPagina] = useState(0);
-
-  useEffect(
-    () => {
-      const url = window.location.href;
-      const res = url.split('?');
-      setPagina(res[1]);
-    }
-  );
-
-  const linksPaginas = (p) => {
-    if (p == 1) {
-      window.open('http://localhost:3000?1', '_self');
-    } else if (p == 2) {
-      window.open('http://localhost:3000?2', '_self');
-    }
-  };
-
-  const retornarPagina = () => {
-    if (pagina == 1) {
-      return <Pagina1 />;
-    } else if (pagina == 2) {
-      return <Pagina2 />;
-    } else {
-      return (
-        <div>
-          <button onClick={() => linksPaginas(1)}> Pagina 1 </button>
-          <button onClick={() => linksPaginas(2)}> Pagina 2 </button>
-        </div>
-      );
-    }
-  };
-
+  const [categoria, setCategoria] = useState('');
 
   return (
     <>
-      {retornarPagina()}
+      {pesquisaCategoria(categoria, setCategoria)}
+      {TabelaCarros(categoria)}
     </>
   );
 };
