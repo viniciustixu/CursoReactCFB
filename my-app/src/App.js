d7d7af27ac75b44f609e3528041c9d67b498eb44
Vinicius Souza <vinicipkt@live.com>
2024-05-23 15:19:22 -0300

    Calculadora de IMC

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,73 +1,104 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 
 
-const carros = [
-  { categoria: "Esporte", preco: "110000", modelo: "Golf GTI" },
-  { categoria: "Esporte", preco: "135000", modelo: "Camaro" },
-  { categoria: "SUV", preco: "70000", modelo: "HRV" },
-  { categoria: "SUV", preco: "85000", modelo: "T-Cross" },
-  { categoria: "Utilitário", preco: "90000", modelo: "Hillux" },
-  { categoria: "Utilitário", preco: "65000", modelo: "Ranger" },
-];
-
-const linhas = (cat) => {
-  const li = [];
-  carros.forEach((carro, index) => {
-    if (carro.categoria.toLocaleUpperCase().includes(cat.toLocaleUpperCase()) || cat === "") {
-      li.push(
-        <tr>
-          <td>{carro.categoria}</td>
-          <td>{carro.preco}</td>
-          <td>{carro.modelo}</td>
-        </tr>
-      );
-    }
-  });
-
-  return li;
-};
-
-
-const TabelaCarros = (cat) => {
+const tabelaIMC = () => {
   return (
-    <table border='1' style={{ borderCollapse: 'collapse' }}>
+    <table border='1' style={{ borderCollapse: 'collapse' }} >
       <thead>
         <tr>
-          <th>Categoria</th>
-          <th>Preço</th>
-          <th>Modelo</th>
+          <th>
+            Classificação
+          </th>
+          <th>
+            IMC
+          </th>
         </tr>
       </thead>
       <tbody>
-        {linhas(cat)}
+        <tr>
+          <td>Abaixo do peso </td>
+          <td>Abaixo de 18,5 </td>
+        </tr>
+        <tr>
+          <td>Peso normal </td>
+          <td>Entre 18,5 e 24,9 </td>
+        </tr>
+        <tr>
+          <td>Sobrepeso </td>
+          <td>Entre 25 e 29,9 </td>
+        </tr>
+        <tr>
+          <td>Obesidade grau I </td>
+          <td>Entre 30 e 34,9 </td>
+        </tr>
+        <tr>
+          <td>Obesidade grau II </td>
+          <td>Entre 35 e 39,9 </td>
+        </tr>
+        <tr>
+          <td>Obesidade grau III ou Mórbida </td>
+          <td>Acima de 40 </td>
+        </tr>
       </tbody>
     </table>
   );
 };
 
-const pesquisaCategoria = (cat, scat) => {
+const fpeso = (p, sp) => {
   return (
     <div>
-      <label htmlFor="idPesquisa">Digite uma categoria</label>
-      <input type="text" id="idPesquisa" value={cat} onChange={(e) => scat(e.target.value)} />
+      <label htmlFor="Ipeso">Peso</label>
+      <input type="text" id="Ipeso" value={p} onChange={(e) => { sp(e.target.value); }} />
     </div>
   );
-
 };
 
+const faltura = (a, sa) => {
+  return (
+    <div>
+      <label htmlFor="Ialtura">Altura</label>
+      <input type="text" id="Ialtura" value={a} onChange={(e) => { sa(e.target.value); }} />
+    </div>
+  );
+};
+
+const fcalcular = (p, a, sr) => {
+  const calc = () => {
+    sr(p / (a * a));
+  };
+
+  return (
+    <div>
+      <button onClick={calc} >Calcular</button>
+    </div>
+  );
+};
+
+const fresultado = (r) => {
+  return (
+    <div>
+      <p>Resultado: {r.toFixed(2)}</p>
+    </div>
+  );
+};
 
 
 function App() {
 
-  const [categoria, setCategoria] = useState('');
+  const [peso, setPeso] = useState(0);
+  const [altura, setAltura] = useState(0);
+  const [reultado, setResultado] = useState(0);
 
   return (
     <>
-      {pesquisaCategoria(categoria, setCategoria)}
-      {TabelaCarros(categoria)}
+      {fpeso(peso, setPeso)}
+      {faltura(altura, setAltura)}
+      {fcalcular(peso, altura, setResultado)}
+      {fresultado(reultado)}
+      {tabelaIMC()}
     </>
   );
 };
