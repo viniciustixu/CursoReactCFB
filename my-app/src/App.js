10ee757da6b7b6b38d80fa76b7f37b0d75d26242
Vinicius Souza <vinicipkt@live.com>
2024-05-27 20:52:18 -0300

    Calculadora em React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,162 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TabelaIMC from './Components/TabelaIMC';
-import Fpeso from './Components/Fpeso';
-import Faltura from './Components/Faltura';
-import Fcalcular from './Components/Fcalcular';
+
+
+// TO DO
+// Impedir mais de um operador na calculadora
+// impedir colocar mais um ponto em um numero que já tenha ponto
+
 
 function App() {
 
-  const [peso, setPeso] = useState(0);
-  const [altura, setAltura] = useState(0);
-  const [reultado, setResultado] = useState(0);
+  const [valorTela, setValorTela] = useState('');
+  const [resultado, setResultado] = useState(0);
+  const [acumulador, setAcumulador] = useState(0);
+  const [operado, setOperado] = useState(false);
+
+
+  //COMPONENTES
+  const Tela = (valor, res) => {
+    return (
+      <div style={cssTela}>
+        <span style={cssTelaOperado}>{valor}</span>
+        <span style={cssTelaRes}>{res}</span>
+      </div>
+    );
+  };
+
+  const Btn = (label, onClick) => {
+    return (
+      <button style={cssBtn} onClick={onClick}>{label}</button>
+    );
+  };
+
+  //FUNCOES
+  const addDigitoTela = (d) => {
+    if ((d == '+' || d == '-' || d == '*' || d == '/') && operado) {
+      console.log("+-*/");
+      setOperado(false);
+      setValorTela(resultado + d);
+      return;
+    } if (operado) {
+      setValorTela(d);
+      setOperado(false);
+      return;
+    }
+    const valorDigitadoTela = valorTela + d;
+    setValorTela(valorDigitadoTela);
+  };
+
+  const limparMemoria = () => {
+    setOperado(false);
+    setValorTela('');
+    setResultado(0);
+    setAcumulador(0);
+    return;
+  };
+
+  const operacao = (oper) => {
+    if (oper == 'bs') {
+      let vtela = valorTela;
+      vtela = vtela.substring(0, (vtela.length - 1));
+      setValorTela(vtela);
+      setOperado(false);
+      return;
+    }
+
+    try {
+      const r = eval(valorTela); //Cálculo
+      setAcumulador(r);
+      setResultado(r);
+      setOperado(true);
+    } catch {
+      setResultado('error');
+    }
+  };
+
+
+
+
+  //ESTILOS
+  const cssContainer = {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'column',
+    width: 300,
+    border: '1px solid black'
+  };
+
+  const cssBotoes = {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  };
+
+  const cssTela = {
+    display: 'flex',
+    paddingLeft: 20,
+    paddingRight: 20,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: '#444',
+    flexDirection: 'column',
+    width: '260px'
+  };
+
+  const cssTelaOperado = {
+    fontSize: 25,
+    color: '#fff',
+    height: 20
+  };
+
+  const cssTelaRes = {
+    fontSize: 50,
+    color: '#fff',
+  };
+
+  const cssBtn = {
+    color: 'white',
+    fontSize: 30,
+    height: 75,
+    width: 75,
+    padding: 20,
+    backgroundColor: '#000',
+    borderColor: '#000',
+    textAlign: 'center',
+    outline: 'none'
+  };
+
+
 
   return (
     <>
-      <Fpeso peso={peso} setPeso={setPeso} />
-      <Faltura altura={altura} setAltura={setAltura} />
-      <Fcalcular peso={peso} altura={altura} setResultado={setResultado} />
-      <TabelaIMC />
+      <div style={cssContainer}>
+        <h3>Calculadora matemática simples</h3>
+        {Tela(valorTela, resultado)}
+        <div style={cssBotoes}>
+          {Btn('AC', limparMemoria)}
+          {Btn('(', () => addDigitoTela('('))}
+          {Btn(')', () => addDigitoTela(')'))}
+          {Btn('/', () => addDigitoTela('/'))}
+          {Btn('7', () => addDigitoTela('7'))}
+          {Btn('8', () => addDigitoTela('8'))}
+          {Btn('9', () => addDigitoTela('9'))}
+          {Btn('*', () => addDigitoTela('*'))}
+          {Btn('4', () => addDigitoTela('4'))}
+          {Btn('5', () => addDigitoTela('5'))}
+          {Btn('6', () => addDigitoTela('6'))}
+          {Btn('-', () => addDigitoTela('-'))}
+          {Btn('1', () => addDigitoTela('1'))}
+          {Btn('2', () => addDigitoTela('2'))}
+          {Btn('3', () => addDigitoTela('3'))}
+          {Btn('+', () => addDigitoTela('+'))}
+          {Btn('0', () => addDigitoTela('0'))}
+          {Btn('.', () => addDigitoTela('.'))}
+          {Btn('<<', () => operacao('bs'))}
+          {Btn('=', () => operacao('='))}
+        </div>
+      </div>
     </>
   );
 };
