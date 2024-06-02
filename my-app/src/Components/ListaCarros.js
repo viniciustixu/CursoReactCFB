0a3d364c9d6347915f197a496b3fc370600c617d
Vinicius Souza <vinicipkt@live.com>
2024-06-02 02:16:30 -0300

    Convertendo Axios para Fetch

--- my-app/src/Components/ListaCarros.js
+++ my-app/src/Components/ListaCarros.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+
 
 export default function ListaCarros() {
 
   const [carros, setCarros] = useState([]);
 
   useEffect(() => {
-    axios.get('https://aa3181fd-69ad-4a4e-84b5-a5204fa73158-00-lq72kyyhqfwq.worf.replit.dev/').then(res => {
-      const dadosCarros = res.data;
-      setCarros(dadosCarros);
-    });
+    fetch('https://aa3181fd-69ad-4a4e-84b5-a5204fa73158-00-lq72kyyhqfwq.worf.replit.dev/')
+      .then(res => res.json())
+      .then(
+        (resultado) => {
+          setCarros(resultado);
+        }
+      );
   });
+
 
 
 
@@ -21,4 +25,4 @@ export default function ListaCarros() {
       )}
     </div>
   );
-}
+};
