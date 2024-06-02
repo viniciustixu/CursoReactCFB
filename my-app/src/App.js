97857fec0c341fecefa29857a45b96027c3e2d75
Vinicius Souza <vinicipkt@live.com>
2024-06-02 06:25:42 -0300

    Aprendenso sobre rotas(incompleto)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,31 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ListaCarros from './Components/ListaCarros';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Pagina1 from './Components/Pagina1';
+import Pagina2 from './Components/Pagina2';
 
 
+/*
+  TO DO:
+  Entender o porque de não estar renderizando nada
+  nas paginas chamadas usando Router Routes Route
+*/
 
-function App() {
 
+export default function App() {
   return (
     <>
-      <ListaCarros />
+      <Router>
+        <header>
+          <Link to="/pagina1">Página 1</Link>
+          <Link to="/pagina2">Página 2</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route exact path="/pagina1" element={<Pagina1 />} />
+            <Route exact path="/pagina2" element={<Pagina2 />} />
+          </Routes>
+        </main>
+      </Router>
     </>
   );
-};
-
-export default App;
+}
