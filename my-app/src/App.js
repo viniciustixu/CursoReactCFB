4fd0c7417dac04684c5ec4ed9ce9d43c0a189e49
Vinicius Souza <vinicipkt@live.com>
2024-06-03 01:19:51 -0300

    Rotas de navegação
    Rotas de navegação + correção de vários problemas de diretório

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Canal from './Components/Canal';
 import Pagina1 from './Components/Pagina1';
 import Pagina2 from './Components/Pagina2';
-
-
-/*
-  TO DO:
-  Entender o porque de não estar renderizando nada
-  nas paginas chamadas usando Router Routes Route
-*/
-
 
 export default function App() {
   return (
     <>
       <Router>
-        <header>
-          <Link to="/pagina1">Página 1</Link>
-          <Link to="/pagina2">Página 2</Link>
-        </header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/Canal">Canal</Link>
+          <Link to="/Pagina1">Página 1</Link>
+          <Link to="/Pagina2">Página 2</Link>
+        </nav>
         <main>
           <Routes>
-            <Route exact path="/pagina1" element={<Pagina1 />} />
-            <Route exact path="/pagina2" element={<Pagina2 />} />
+            <Route path="/Canal" element={<Canal />} />
+            <Route path="/Pagina1" element={<Pagina1 />} />
+            <Route path="/Pagina2" element={<Pagina2 />} />
           </Routes>
         </main>
       </Router>
--- my-app/src/Components/Canal.js
+++ my-app/src/Components/Canal.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Canal() {
   return (
-    <h1>H1</h1>
+    <h1>Canal</h1>
   );
 }
