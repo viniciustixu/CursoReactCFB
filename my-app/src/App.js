9aae69adae6e6ffc431bd9b879a15884fb40c4ec
Vinicius Souza <vinicipkt@live.com>
2024-06-03 01:57:20 -0300

    Atributo Key

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Canal from './Components/Canal';
-import Pagina1 from './Components/Pagina1';
-import Pagina2 from './Components/Pagina2';
+
+
+
+function ListaNumeros(props) {
+  const num = props.numeros;
+  const listinha = num.map(
+    (n) => <li key={n}> {n} </li>
+  );
+  return (
+    <ul>{listinha}</ul>
+  );
+}
+
+const array_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export default function App() {
   return (
     <>
-      <Router>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/Canal">Canal</Link>
-          <Link to="/Pagina1">Página 1</Link>
-          <Link to="/Pagina2">Página 2</Link>
-        </nav>
-        <main>
-          <Routes>
-            <Route path="/Canal" element={<Canal />} />
-            <Route path="/Pagina1" element={<Pagina1 />} />
-            <Route path="/Pagina2" element={<Pagina2 />} />
-          </Routes>
-        </main>
-      </Router>
+      <ListaNumeros numeros={array_numeros} />
     </>
   );
 }
